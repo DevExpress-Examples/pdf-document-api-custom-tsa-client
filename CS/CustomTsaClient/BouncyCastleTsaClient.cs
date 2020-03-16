8e8c0cfa86b0143e50115fbf4b3f0f1f6261c8a7
geumjaLee <polina.fedorova@devexpress.com>
2020-03-16 16:54:10 +0300

    add cs files

---
+++ CS/CustomTsaClient/BouncyCastleTsaClient.cs
@@ -0,0 +1,85 @@
+﻿using DevExpress.Pdf;
+using Org.BouncyCastle.Asn1.Cmp;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Tsp;
+using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace CustomTsaClient
+{
+    public class BouncyCastleTsaClient : ITsaClient
+    {
+        readonly Uri tsaServerURI;
+
+        readonly IDigest hashCalculator;
+
+        public BouncyCastleTsaClient(Uri tsaServerURI, IDigest hashCalculator)
+        {
+            this.tsaServerURI = tsaServerURI;
+            this.hashCalculator = hashCalculator;
+        }
+        byte[] CalculateDigest(Stream stream)
+        {
+            byte[] buffer = new byte[81920];
+            hashCalculator.Reset();
+            int bytesRead;
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                hashCalculator.BlockUpdate(buffer, 0, bytesRead);
+            byte[] result = new byte[hashCalculator.GetDigestSize()];
+            hashCalculator.DoFinal(result, 0);
+            return result;
+        }
+        
+        public byte[] GenerateTimeStamp(Stream stream)
+        {
+            //Generate a timestamp request:
+            TimeStampRequestGenerator tsqGenerator = new TimeStampRequestGenerator();
+            tsqGenerator.SetCertReq(true);
+            BigInteger nonce;
+            using (RandomNumberGenerator generator = RandomNumberGenerator.Create())
+            {
+                byte[] nonceValue = new byte[10];
+                generator.GetBytes(nonceValue);
+                nonce = new BigInteger(nonceValue);
+            }
+            string algorithmOid = DigestUtilities.GetObjectIdentifier(hashCalculator.AlgorithmName).Id;
+            TimeStampRequest request = tsqGenerator.Generate(algorithmOid, CalculateDigest(stream), nonce);
+            byte[] requestBytes = request.GetEncoded();
+            
+            //Send the request to a server:
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(tsaServerURI);
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "application/timestamp-query";
+            httpRequest.ContentLength = requestBytes.Length;
+            using (Stream requestStream = httpRequest.GetRequestStream())
+                requestStream.Write(requestBytes, 0, requestBytes.Length);
+            
+            //Get a responce from the server:
+            HttpWebResponse httpResponce = (HttpWebResponse)httpRequest.GetResponse();
+            using (Stream respStream = new BufferedStream(httpResponce.GetResponseStream()))
+            {
+                //Read the responce:
+                TimeStampResponse response = new TimeStampResponse(respStream);
+                response.Validate(request);
+                PkiFailureInfo failure = response.GetFailInfo();
+                
+                //Throw an exception if the responce returned an error:                
+                if (failure != null)
+                    throw new Exception($"TimeStamp request to the \"{tsaServerURI}\" failed.");
+                TimeStampToken token = response.TimeStampToken;
+                
+                //Throw an exception if the responce doesn't contain the timestamp:
+                if (token == null)
+                    throw new Exception($"TimeStamp request to the \"{tsaServerURI}\" failed.");
+                return token.GetEncoded();
+            }
+        }
+    }
+
+}
---
+++ CS/CustomTsaClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Pdf;
+using Org.BouncyCastle.Crypto.Digests;
+using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace CustomTsaClient
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var signer = new PdfDocumentSigner(@"Document.pdf"))
+            {
+                //Create a custom timestamp client instance:
+                ITsaClient tsaClient = new BouncyCastleTsaClient(new Uri(@"http://timestamp.apple.com/ts01"), new Sha256Digest());
+
+                //Create a PKCS#7 signature:
+                Pkcs7Signer pkcs7Signature = new Pkcs7Signer(@"testcert.pfx", "123", PdfHashAlgorithm.SHA256, tsaClient);                
+                
+                //Apply the signature to the form field:
+                var signatureBuilder = new PdfSignatureBuilder(pkcs7Signature, "Sign");
+                
+                //Specify image data and signer information:
+                signatureBuilder.SetImageData(File.ReadAllBytes("JaneCooper.jpg"));
+                signatureBuilder.Location = "United Kingdom";
+                
+                //Sign and save the document:
+                signer.SaveDocument("SignedDocument.pdf", signatureBuilder);
+                Process.Start("SignedDocument.pdf");
+            }
+            return;
+        }
+    }
+}
